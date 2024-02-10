abe8897de3061e342d7eb8482efbc6d68ead9966
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-02-11 00:30:19 +0530

    cors

--- server.js
+++ server.js
@@ -9,21 +9,12 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 connectDB()
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-//serve frontend
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend' ,'build', 'index.html')))
-// }
-// else
-// {
-//     app.get('/', (req,res) => res.send('Please set to Poduction'))
-// }
 
 app.use(errorHandler)
 app.listen(port, ()=> console.log(`server started on port ${port}`))
